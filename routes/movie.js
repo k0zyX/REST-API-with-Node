4a9f1f3ef0012838d3b19b8590bc804a365928d1
= <=>
2020-07-05 21:42:40 +0300

    Get-Put-Delete Routes Added and bişiler bişiler

--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,73 @@ const router = express.Router();
 // Models
 const Movie = require("../models/Movie");
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({ });
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id',(req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie)
+      next({message:"movie nat found"});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Guncelleme
+router.put('/:movie_id', async (req, res) => {
+  const update = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {new:true}
+    );
+
+  promise.then((movie) => {
+    if(!movie)
+      next({message:"movie nat found"});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+
+/*
+Selimcan
+
+router.put('/:movie_id', async (req, res) => {
+    const update = await Movie.findByIdAndUpdate(
+        req.params.movie_id,
+        req.body
+    )
+    console.log(update)
+    return req.json({"msg": update })
+})
+
+*/
+
+router.delete('/:movie_id',(req, res, next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie)
+      next({message:"movie nat found"});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+
 router.post('/', (req, res, next) => {
   const {title,imdb_score, category, country, year} = req.body;
 
